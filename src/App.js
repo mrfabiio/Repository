7b6f0e39122f2f6e4cf830e32147bf9145969e07
mrfabiio <mr.fabiio@hotmail.com>
2020-08-27 12:56:05 -0300

    Didmount Refatorado

--- src/App.js
+++ src/App.js
@@ -1,46 +1,13 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
+import React, { Component} from 'react';
 import './App.css';
-import { render } from 'react-dom';
-
-class MyButton extends Component{
+import Repositories from "./containers/Repositories";
+class App extends Component{
   render(){
-    return (
-      <button
-        onClick={()=> {this.props.handleClick(this.props.label);}}
-  > 
-    {this.props.label}
-    </button>
-    );
+    return(
+       <div className="App">
+          <Repositories/>
+        </div>
+    )
   }
 }
-class MyLabel extends Component{
-  render(){
-    return <p>Clique no: {this.props.text}</p>
-  }
-}
-class App extends Component {
-  constructor (props){
-    super (props);
-    
-    this.state={      
-    labelText:'',
-    };
-  }  
-      setlabelText =(labelText) =>{
-        this.setState({labelText})     
-    }
-  render() {
-    console.log(this.state);
-    return (
-    <div className="App">
-    <MyLabel text={this.state.labelText}/>
-    <MyButton handleClick={this.setlabelText} label="Botão1"/>
-    <MyButton handleClick={this.setlabelText} label="Botão2"/>
-    <MyButton handleClick={this.setlabelText} label="botão3"/>
-    <MyButton handleClick={this.setlabelText} label="botão4"/>
-    </div>
-    );
-  }
-}     
 export default App;
